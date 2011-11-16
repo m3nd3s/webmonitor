b4a54d129531efcd9d5d05c16108f95759ec097b
Almir 'm3nd3s <m3nd3s@gmail.com>
2011-11-16 11:09:26 -0200

    a

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,7 +21,7 @@ Webmonitor::Application.configure do
   config.assets.digest = true
 
   # Defaults to Rails.root.join("public/assets")
-  # config.assets.manifest = YOUR_PATH
+  config.assets.manifest = YOUR_PATH
 
   # Specifies the header that your server uses for sending files
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
@@ -43,7 +43,7 @@ Webmonitor::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( bootstrap.css  )
+  config.assets.precompile += %w( bootstrap* highcharts.js  )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
